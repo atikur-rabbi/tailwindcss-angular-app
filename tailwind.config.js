54b37d15f4e3ce08fae79c179caf93f599fed0e8
atikur-rabbi <msawon94@gmail.com>
2021-04-21 13:04:42 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const enablePurge = process.env.ENABLE_PURGE || true;
+module.exports = {
+  purge: [],
+  purge: {
+    enabled: enablePurge,
+    content: [
+      './src/**/*.html',
+      './src/**/*.scss'
+    ]
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
